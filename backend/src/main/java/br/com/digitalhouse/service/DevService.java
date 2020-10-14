c587dc3e3bdd71f120883606bc8d1372d19c0074
thais <thaisw@gmail.com>
2020-10-14 01:13:10 -0300

    criado service de cadastro dev

--- backend/src/main/java/br/com/digitalhouse/service/DevService.java
+++ backend/src/main/java/br/com/digitalhouse/service/DevService.java
@@ -1,54 +1,47 @@
-package br.com.digitalhouse.controller;
+package br.com.digitalhouse.service;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import br.com.digitalhouse.model.Dev;
 import br.com.digitalhouse.repository.DevRepository;
-import br.com.digitalhouse.service.DevService;
 
-@CrossOrigin
-@RestController
-@RequestMapping("/dev")
-public class DevController {
+@Service
+public class DevService {
 	
 	@Autowired
-	private DevService service;
+	private DevRepository repository;
 	
 	@GetMapping
 	public List<Dev> getAllDevs() {
-		return service.getAllDevs();
+		return repository.findAll();
 	}
 	
 	@GetMapping("/{id}")
 	public Dev getDevByID(@PathVariable Long id) {
-		return service.getDevByID(id);
+		return repository.findById(id).orElse(null);
 	}
 	
 	@PostMapping
 	public void addDev(@RequestBody Dev dev) {
-		service.addDev(dev);
+		repository.save(dev);
 	}
 	
 	@DeleteMapping("/{id}")
 	public void deleteDevByID(@PathVariable Long id) {
-		service.deleteDevByID(id);
+		repository.deleteById(id);
 	}
 	
 	@PutMapping("/{id}")
 	public void setDevByID(@PathVariable Long id, @RequestBody Dev dev) {
 
 	}
-
 }
-
