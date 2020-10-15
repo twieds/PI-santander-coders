c0dc56bac1e7c9f3471beb38052127bfbdea3374
thais <thaisw@gmail.com>
2020-10-15 00:17:06 -0300

    componente de detalhe dos projetos

---
+++ frontend/src/app/detalhe-projeto/detalhe-projeto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe-projeto',
+  templateUrl: './detalhe-projeto.component.html',
+  styleUrls: ['./detalhe-projeto.component.css']
+})
+export class DetalheProjetoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
