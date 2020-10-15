5c7ec154bfd371d5b5bd16f87b781d9aa5e7e903
thais <thaisw@gmail.com>
2020-10-15 00:16:52 -0300

    componente de detalhe do desenvolvedor

---
+++ frontend/src/app/detalhe-dev/detalhe-dev.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe-dev',
+  templateUrl: './detalhe-dev.component.html',
+  styleUrls: ['./detalhe-dev.component.css']
+})
+export class DetalheDevComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
