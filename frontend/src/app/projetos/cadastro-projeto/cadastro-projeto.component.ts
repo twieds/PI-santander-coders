6e0fd01f2f90156454a02c6e0a38347d97e0fea7
thais <thaisw@gmail.com>
2020-10-18 00:58:27 -0300

    criado POST do cadastro de projetos

--- frontend/src/app/projetos/cadastro-projeto/cadastro-projeto.component.ts
+++ frontend/src/app/projetos/cadastro-projeto/cadastro-projeto.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ProjetoService } from '../projeto.service';
 
 @Component({
   selector: 'app-cadastro-projeto',
   templateUrl: './cadastro-projeto.component.html',
   styleUrls: ['./cadastro-projeto.component.css']
 })
+
+// TODO: add methods put and delete
 export class CadastroProjetoComponent implements OnInit {
+  
+  form: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private service: ProjetoService) { }
 
+  // TODO: add validators  
   ngOnInit(): void {
+    this.form = this.fb.group({
+      title: [''],
+      description: [''],
+      deadline: [''],
+      category: ['']
+    })
   }
 
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.form.value)
+    if (this.form.valid) {
+      console.log('submit');
+      this.service.create(this.form.value).subscribe(
+        success => console.log('sucesso'),
+        error => console.error(error),
+        () => console.log('request completo')
+      );
+    }
+  }
+
+  onCancel() {
+    this.submitted = false;
+    this.form.reset();
+  }
 }
