661479c1e656c854ee7d8aacfcb12a0d49327d92
thais <thaisw@gmail.com>
2020-12-02 00:45:50 -0300

    novos headers

--- frontend/src/app/headers/header-nav-primary/header-nav-primary.component.ts
+++ frontend/src/app/headers/header-nav-primary/header-nav-primary.component.ts
@@ -2,40 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 
 @Component({
-  selector: 'app-header-nav-primary',
-  templateUrl: './header-nav-primary.component.html',
-  styleUrls: ['./header-nav-primary.component.css']
+    selector: 'app-header-nav-primary',
+    templateUrl: './header-nav-primary.component.html',
+    styleUrls: ['./header-nav-primary.component.css']
 })
 export class HeaderNavPrimaryComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  items: MenuItem[];
+    items: MenuItem[];
 
     ngOnInit() {
         this.items = [
             {
-                label: 'File',
-                items: [{
-                        label: 'New', 
-                        icon: 'pi pi-fw pi-plus',
-                        items: [
-                            {label: 'Project'},
-                            {label: 'Other'},
-                        ]
-                    },
-                    {label: 'Open'},
-                    {label: 'Quit'}
-                ]
+                label: 'HOME',
+                routerLink: ['/home']
             },
             {
-                label: 'Edit',
-                icon: 'pi pi-fw pi-pencil',
+                label: 'QUEM SOMOS',
+                routerLink: ['/home'],
+            },
+            {
+                label: 'PROCURAR',
                 items: [
-                    {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-                    {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+                    { label: 'Devs', icon: 'pi pi-fw pi-desktop', routerLink: ['/dev-list']},
+                    { label: 'Ongs', icon: 'pi pi-fw pi-heart', routerLink: ['/ngo-list'] },
+                    { label: 'Projetos', icon: 'pi pi-fw pi-briefcase', routerLink: ['/project-list'] }
                 ]
-            }
+            },
+            // {
+            //     label: 'Edit',
+            //     icon: 'pi pi-fw pi-pencil',
+            //     items: [
+            //         {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+            //         {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+            //     ]
+            // }
         ];
     }
 }
